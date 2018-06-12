3adf3429511d17c8146eb7694785f6d14d535a7b
nithish-knetworks <nithish@knetworks.co.in>
2018-06-12 07:55:30 -0500

    Ameresco usa sync install date alone

--- src/main/java/com/terragoedge/streetlight/StreetlightApp.java
+++ src/main/java/com/terragoedge/streetlight/StreetlightApp.java
@@ -1,24 +1,16 @@
 package com.terragoedge.streetlight;
 
+import com.terragoedge.streetlight.service.InstallDateFix;
 import org.apache.log4j.Logger;
 
 import com.terragoedge.streetlight.service.StreetlightChicagoService;
 
 public class StreetlightApp {
 	
-	final static Logger logger = Logger.getLogger(StreetlightApp.class);
-	
+
 	public static void main(String[] args) {
-		StreetlightChicagoService streetlightChicagoService = new StreetlightChicagoService();
-		while(true){
-			try{
-				streetlightChicagoService.run();
-				Thread.sleep(60000);
-				
-			}catch(Exception e){
-				logger.error("Error in main", e);
-			}
-		}
+		InstallDateFix streetlightChicagoService = new InstallDateFix();
+		streetlightChicagoService.run();
 
 	}
 
