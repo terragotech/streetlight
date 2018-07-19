2fbbaa1bb4300b650dde7e6821b8b2f08638ba4f
nithish-knetworks <nithish@knetworks.co.in>
2018-07-19 15:50:51 +0530

    Configuration json files added

---
+++ src/main/java/com/terragoedge/slvinterface/json/slvInterface/Action.java
@@ -0,0 +1,22 @@
+package com.terragoedge.slvinterface.json.slvInterface;
+
+public class Action {
+    private Integer id;
+    private String value;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
