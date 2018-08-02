ee96009febba816762d9369718c7c3e5fb0aa5ab
nithish-knetworks <nithish@knetworks.co.in>
2018-08-02 20:52:52 +0530

    added

--- src/main/java/com/terragoedge/slvinterface/service/AmerescoReportService.java
+++ src/main/java/com/terragoedge/slvinterface/service/AmerescoReportService.java
@@ -1,15 +1,20 @@
 package com.terragoedge.slvinterface.service;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.terragoedge.slvinterface.model.CsvReportModel;
+import com.terragoedge.slvinterface.model.EdgeFormData;
 import com.terragoedge.slvinterface.model.EdgeNote;
+import com.terragoedge.slvinterface.utils.Constants;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class AmerescoReportService {
+public class AmerescoReportService extends AbstractService {
+    public Gson gson = null;
 
     public AmerescoReportService() {
-
+        gson = new Gson();
     }
 
     public void start() {
@@ -23,6 +28,23 @@ public class AmerescoReportService {
     }
 
     public void processEdgeNote(EdgeNote edgeNote, CsvReportModel csvReportModel) {
+
+    }
+
+    public void processInspectionForm(String formDef, CsvReportModel csvReportModel) {
+        List<EdgeFormData> edgeFormDataList = getEdgeFormData(formDef);
+        String issueType = null;
+        String comments = null;
+        try {
+            issueType = getValueByLabel(edgeFormDataList, Constants.ISSUE_TYPE);
+            comments = getValueByLabel(edgeFormDataList, Constants.COMMENT);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        csvReportModel.setIssue1(issueType);
+        csvReportModel.setAddComment1(comments);
+    }
+    public void setCsvModel(EdgeNote edgeNote,CsvReportModel csvReportModel){
 
     }
 }
