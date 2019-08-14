4bde919ed27a729d3f916d6aa2cd3b4e076a5066
Nithish <nitish@knetworks.co.in>
2019-08-14 07:21:46 -0500

    Head to Head wiring lable changes

--- src/main/java/com/terragoedge/streetlight/installmaintain/InstallMaintenanceDao.java
+++ src/main/java/com/terragoedge/streetlight/installmaintain/InstallMaintenanceDao.java
@@ -273,7 +273,7 @@ public class InstallMaintenanceDao extends UtilDao {
                 CSVWriter.NO_ESCAPE_CHARACTER,
                 CSVWriter.DEFAULT_LINE_END);
         String[] headerRecord = {"Title", "Action", "MAC Address", "User Id", "Fixture QR Scan", "Fixture Type",
-                "Context", "Lat", "Lng", "Date Time", "Is ReplaceNode", "Existing Node MAC Address", "New Node MAC Address", "New Fixture QR Scan", "Reason for Replacement", "Reason for removal", "Resolved Issue", "Resolved Comment", "Scan Existing MAC if wrong", "UnableToRepair Issue", "unableToRepair Comment", "InstallStatus", "Skipped Fixture Reason", "Skipped Reason","Head-to-head"};
+                "Context", "Lat", "Lng", "Date Time", "Is ReplaceNode", "Existing Node MAC Address", "New Node MAC Address", "New Fixture QR Scan", "Reason for Replacement", "Reason for removal", "Resolved Issue", "Resolved Comment", "Scan Existing MAC if wrong", "UnableToRepair Issue", "unableToRepair Comment", "InstallStatus", "Skipped Fixture Reason", "Skipped Reason","Head-to-head wiring installation"};
         csvWriter.writeNext(headerRecord);
         return csvWriter;
     }
