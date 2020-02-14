b0066db7c0f00c08793db77d824cd79442e07d58
nithish-knetworks <nitish@knetworks.co.in>
2020-02-14 11:40:53 +0000

    Version increased

--- src/main/java/com/slvinterface/utils/ResourceDetails.java
+++ src/main/java/com/slvinterface/utils/ResourceDetails.java
@@ -1,8 +1,8 @@
 package com.slvinterface.utils;
 
 public class ResourceDetails {
-    //public static final String CONFIG_JSON_PATH = "./resources/config.json";
-   public static final String CONFIG_JSON_PATH = "./src/main/resources/surrey/config.json";
-    public static final String PROPS_PATH = "./src/main/resources/surrey/main.properties";
-  //  public static final String PROPS_PATH = "./resources/main.properties";
+    public static final String CONFIG_JSON_PATH = "./resources/config.json";
+   //public static final String CONFIG_JSON_PATH = "./src/main/resources/surrey/config.json";
+   // public static final String PROPS_PATH = "./src/main/resources/surrey/main.properties";
+    public static final String PROPS_PATH = "./resources/main.properties";
 }
