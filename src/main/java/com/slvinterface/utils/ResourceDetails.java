39af2b7680d723f5777a8686ec31c1c255d19a9f
nithish-knetworks <nitish@knetworks.co.in>
2020-10-30 13:21:08 +0000

    Update ResourceDetails.java

--- src/main/java/com/slvinterface/utils/ResourceDetails.java
+++ src/main/java/com/slvinterface/utils/ResourceDetails.java
@@ -1,11 +1,11 @@
 package com.slvinterface.utils;
 
 public class ResourceDetails {
-   // public static final String GENERIC_CONFIG = "./resources/config.json";
+    public static final String GENERIC_CONFIG = "./resources/mapping.json";
     public static final String CONFIG_JSON_PATH = "./resources/config.json";
     public static final String CONFIG_JSON_PATH1 = "./resources/config1.json";
    //public static final String CONFIG_JSON_PATH = "./src/main/resources/config.json";
-    public static final String PROPS_PATH = "./src/main/resources/main.properties";
-    public static final String GENERIC_CONFIG = "./src/main/resources/mapping.json";
-   // public static final String PROPS_PATH = "./resources/main.properties";
+   // public static final String PROPS_PATH = "./src/main/resources/main.properties";
+   // public static final String GENERIC_CONFIG = "./src/main/resources/mapping.json";
+    public static final String PROPS_PATH = "./resources/main.properties";
 }
