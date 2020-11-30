96a36cdc6a0a3df5b07551c758c650685ae6598d
nithish-knetworks <nitish@knetworks.co.in>
2020-11-30 11:54:55 +0000

    Duke Pilot has been configured

--- src/main/java/com/slvinterface/entity/SLVSyncTable.java
+++ src/main/java/com/slvinterface/entity/SLVSyncTable.java
@@ -3,7 +3,7 @@ package com.slvinterface.entity;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
-@DatabaseTable(tableName = "slvsyncdetails_vancouver")
+@DatabaseTable(tableName = "slvsyncdetails_duke")
 public class SLVSyncTable {
 
     public static final String NOTE_GUID = "noteguid";
